471144a70c9c0065cdacaa418f655a5f37cfcdeb
anastasiia <asiamewwzi99@gmail.com>
2024-12-12 14:54:23 +0300

    lab7

--- scripts.js
+++ scripts.js
@@ -102,3 +102,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     fetchUserData();
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('#gallery', {
+        loop: true,
+        slidesPerView: 3,
+        spaceBetween: 20,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        mousewheel: true,
+        grabCursor: true,
+    });
+});
+
